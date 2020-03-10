1c267f24535bcc78c547ed391970d833ce2f48e1
dvrci <nimet_divarci@hotmail.com>
2020-03-10 14:59:45 +0100

    adding hamburger menu
    Did this in seperate branch which I dont want to merge to master. This messed things up, more details on CF

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 console.log('Hey there! Wellcome to my Portfolio Site!')
 var text2 = null;
 var text3 = 'Single quotes are valid too.';
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
